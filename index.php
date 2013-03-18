fbd8407d752b4477e990accb119ec9246f28d40a
Cristoforo Magliozzi <Cristoforo@Magbook-Pro-20.local>
2013-03-18 12:43:26 -0400

    video

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8"/>
+	<title>Guest & Host</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+		<video id="vid" width:"1280" height:"800" autoplay loop>
+			<source src="/vid/home.mp4" type="video/mp4"/>
+			<!--
+							<source src="vid/The_Guys.ogv" type="video/ogg"/>
+							<source src="vid/The_Guys.webm" type="video/webm"/>
+						-->
+		</video>
+</body>
+
+</html>
