18f2f4f94eb9fd699723db5fda53a2d91543a5eb
Cristoforo Magliozzi <CMagliozzi@post.harvard.edu>
2013-03-18 13:14:30 -0400

    background

--- index.php
+++ index.php
@@ -7,12 +7,14 @@
 	<link rel="stylesheet" href="css/main.css">
 </head>
 
-<body>
-		<video id="vid" width:"1280" height:"800" autoplay loop>
-			<source src="vid/home.mp4" type="video/mp4"/>
-			<source src="vid/home.ogv" type="video/ogg"/>
-			<source src="vid/home.webm" type="video/webm"/>
-		</video>
+<body style="background-color:black";>
+		
+			<video id="vid" width:"1280" height:"800" autoplay loop>
+				<source src="vid/home.mp4" type="video/mp4"/>
+				<source src="vid/home.ogv" type="video/ogg"/>
+				<source src="vid/home.webm" type="video/webm"/>
+			</video>
+	
 </body>
 
 </html>
