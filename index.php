755fc5350e2438ac72ffc63145c028f533882b47
Cristoforo Magliozzi <CMagliozzi@post.harvard.edu>
2013-03-20 12:09:07 -0400

    broader vid support

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
 
 		<video id="vid" width:"270" height:"800" autoplay loop>
 					<source src="vid/page.mp4" type="video/mp4"/>
-					<!-- <source src="vid/home.ogv" type="video/ogg"/>
-					<source src="vid/home.webm" type="video/webm"/> -->
+					<source src="vid/page.ogv" type="video/ogg"/>
+					<source src="vid/page.webm" type="video/webm"/>
 				</video>
 		
 		<div id="pic"> </div>
