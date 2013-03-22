17e5126a61cb3d71f78f1f80306726d7efe5d3b3
Cristoforo Magliozzi <CMagliozzi@post.harvard.edu>
2013-03-22 12:54:13 -0400

    modal video

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1" />
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/main.css">
+	<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
 </head>
 
 <body>
@@ -19,7 +20,23 @@
 
 		<img id="page" src="img/bg.jpg"/>
 		
-		<div id="name" class="ghost"></div>	
+		<div>
+			<div id="name" class="ghost"></div>
+			<div>
+				<label for="toggle"></label>
+				<input type="checkbox" id="toggle" />
+				 <div id="modal">
+					 <video class="overlay" autoplay>
+							<source src="./vid/name.mp4" type="video/mp4"/>
+						<!--
+							<source src="vid/The_Guys.ogv" type="video/ogg"/>
+							<source src="vid/The_Guys.webm" type="video/webm"/>
+						-->
+					</video>
+				</div>
+			</div>
+		</div>
+			
 		
 	  
 	
