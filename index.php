fc20f605f93f4ebdc79b84c412053a508749064b
Cristoforo Magliozzi <CMagliozzi@post.harvard.edu>
2013-03-22 11:34:37 -0400

    animated link

--- index.php
+++ index.php
@@ -5,14 +5,25 @@
 	<meta charset="utf-8"/>
 	<title>Guest & Host</title>
 	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1" />
+	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
 
-<div id="container">
+<div id="wrapper">
 
-<img id="page" src="img/bg.jpg"/>	
+	<div id="content">
+	
+		
+
+		<img id="page" src="img/bg.jpg"/>
+		
+		<div id="name" class="ghost"></div>	
+		
+	  
+	
+	</div>                                        
 
 </div>
 	
