c729a5d1be8661af3e7e3c5cd4d9290e429c3cbd
Cristoforo Magliozzi <CMagliozzi@post.harvard.edu>
2013-03-24 17:04:26 -0400

    fullscreen video?

--- name.php
+++ name.php
@@ -7,28 +7,16 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/video.css">
 </head>
 
 <body>
-
-<div id="wrapper">
-
-	<div id="content">
-	
-
-      <video controls>
+      <video id="v" controls>
 			<source src="./vid/name.mp4" poster="./img/intro.png" type="video/mp4"/>
 			<source src="vid/name.ogv" type="video/ogg"/>
 			<source src="vid/name.webm" type="video/webm"/>
 		
-	  </video>
-	
-	</div>                                        
-
-</div>
-	
-
-
+	  </video>                             
 </body>
 
 </html>
