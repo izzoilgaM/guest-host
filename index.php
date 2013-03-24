7270cef6b230ae2e8fa9438d39b9db4703a01e34
Cristoforo Magliozzi <CMagliozzi@post.harvard.edu>
2013-03-24 13:17:27 -0400

    responsive video

--- index.php
+++ index.php
@@ -26,8 +26,8 @@
 					<label for="toggle"></label>
                     <input type="checkbox" id="toggle"/>
                     <div id="modal">	
-				          <video class="overlay" autoplay controls>
-								<source src="./vid/name.mp4" height="1024" width="576" poster="./img/intro.png" type="video/mp4"/>
+				          <video class="overlay" controls>
+								<source src="./vid/name.mp4" poster="./img/intro.png" type="video/mp4"/>
 							<!--
 								<source src="vid/The_Guys.ogv" type="video/ogg"/>
 								<source src="vid/The_Guys.webm" type="video/webm"/>
