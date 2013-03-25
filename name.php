16725d2d2ad737cd6082e1ee524b8d843d66c862
Cristoforo Magliozzi <CMagliozzi@post.harvard.edu>
2013-03-24 22:08:58 -0400

    revert

--- name.php
+++ name.php
@@ -8,15 +8,26 @@
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/main.css">
 	<link rel="stylesheet" href="css/video.css">
-</head>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
+	</head>
 
 <body>
-      <video id="v" controls>
+		<video class="v" controls
+		  preload="auto" width="100%" height="100%" poster="./img/intro.png">
+		  <source src="./vid/name.mp4" type='video/mp4'>
+		  <source src="vid/name.ogv" type="video/ogg"/>
+		  <source src="./vid/name.webm" type='video/webm'>
+		</video>
+
+<!--
+      <video id="jquery_jplayer_1" class="v" controls>
 			<source src="./vid/name.mp4" poster="./img/intro.png" type="video/mp4"/>
 			<source src="vid/name.ogv" type="video/ogg"/>
 			<source src="vid/name.webm" type="video/webm"/>
 		
-	  </video>                             
+	  </video>   
+--> 
+                        
 </body>
 
 </html>
