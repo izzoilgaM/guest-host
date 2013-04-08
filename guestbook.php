e1b5d4c2b5939346d3b8b1841880684434d16720
Cristoforo Magliozzi <CMagliozzi@post.harvard.edu>
2013-04-08 17:11:04 -0400

    guestbook styled

--- guestbook.php
+++ guestbook.php
@@ -9,7 +9,13 @@
 	<link rel="stylesheet" href="css/main.css">
 </head>
 
-<body id="guestbook"> 
+<body> 
+
+<div id="wrapper">
+
+	<div id="guestbook">
+	
+		<div id="comments">
 
 <!-- begin htmlcommentbox.com -->
  <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Guest & Host</a> is loading comments...</div>
@@ -17,6 +23,11 @@
  <script type="text/javascript" language="javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"),l=(""+window.location || hcb_user.PAGE), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+escape(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24DDyfuJGViRVmmzOyfst10/"+"&opts=16862&num=10");if (typeof s!="undefined")document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
 <!-- end htmlcommentbox.com -->
 
+		</div>
+
+	</div>
+
+</div>
 
 </body>
 
