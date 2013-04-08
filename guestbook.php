9cefcd160666cfc8b86f31f07c500503a42a1f87
Cristoforo Magliozzi <CMagliozzi@post.harvard.edu>
2013-04-08 16:49:36 -0400

    update

--- guestbook.php
+++ guestbook.php
@@ -9,10 +9,10 @@
 	<link rel="stylesheet" href="css/main.css">
 </head>
 
-<body style="padding:20px; width:1024px; height:768px; color:white">
+<body id="guestbook"> 
 
 <!-- begin htmlcommentbox.com -->
- <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">HTML Comment Box</a> is loading comments...</div>
+ <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Guest & Host</a> is loading comments...</div>
  <link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
  <script type="text/javascript" language="javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"),l=(""+window.location || hcb_user.PAGE), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+escape(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24DDyfuJGViRVmmzOyfst10/"+"&opts=16862&num=10");if (typeof s!="undefined")document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
 <!-- end htmlcommentbox.com -->
