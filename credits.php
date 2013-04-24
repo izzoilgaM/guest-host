009b8547a797f9775a0736330b6c6e20f7e64506
Cristoforo Magliozzi <CMagliozzi@post.harvard.edu>
2013-04-24 09:51:08 -0400

    credits and analytics

---
+++ credits.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8"/>
+	<title>Guest & Host</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700' rel='stylesheet' type='text/css'>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40385079-1', 'herokuapp.com');
+  ga('send', 'pageview');
+
+</script>
+</head>
+
+<body> 
+
+<div id="wrapper">
+
+	<div id="guestbook">
+	
+		<div>
+			<a id="home" href="http://guestandhost.herokuapp.com">  </a>
+		</div>
+		
+		<div id="credits_title">
+		<h1> Credits </h1>
+		</div>
+	
+		<div id="credits">
+<p>
+Built for the Guest and Host exhibition at the Tel Aviv Museum of Art, GHOST is a composite formed out of the words that make up the exhibition's title. It consists in six multimedia montages built on the Zeega interactive storytelling platform. Entitled Footing, Bed, Wall, Hotel, What’s in a name? and Carrier, they form less a cohesive set than a scatterplot of seeds harvested from across the digital landscape, assembled on the fly within the quires of a virtual Guest Book. The Guest Book is an empty container full of found materials: texts, photographs, recordings, animations. Instead of being downloaded, these are served up live from remote online hosts. Wrenched from familiar surroundings into ones where they wander like strangers in a strange land, they arrive stitched together in real time.
+<br><br>
+Like the places of passage that make up the contemporary economy of hospitality—the hotels, hospitals, hospices, and hostels that take us in and where life’s scripted plans collide with its hazards and accidents—Ghost operates inside the walls of the public-private home of the World Wide Web with its electronic handshakes and data protocols.
+<br><br>
+<b> Artist/Designer - <a href="http://jeffreyschnapp.com/">Jeffrey Schnapp</a>, <a href="http://metalab.harvard.edu/">metaLAB (at) Harvard</a></b>
+<br><br>
+<b> Video/web production and production support - <a href="http://cristoforomagliozzi.com"> Cristoforo Magliozzi, <a href="http://metalab.harvard.edu/">metaLAB (at) Harvard</a> </b>
+<br><br>
+<b> Technical assistance - <a href="http://zeega.com/"> the Zeega team</a> </b>
+</p>
+		</div>
+		
+		
+
+	</div>
+
+</div>
+
+</body>
+
+</html>
